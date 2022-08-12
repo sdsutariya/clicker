a6f6cbc9d13563e17e320f915f613742cf3a1fe5
Shivam Kahar <shivam.kahar@silentinfotech.com>
2022-08-12 17:21:14 +0530

    Frontend setup

---
+++ clicker-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box>
+      <Typography variant='h1'>Hello!</Typography>
+    </Box>
+  );
+}
+
+export default App;
